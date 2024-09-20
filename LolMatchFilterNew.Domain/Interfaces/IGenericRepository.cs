fe7a9f850e16926bbefd2696e9828a1b1df2aab5
Mchapman <mchapman6292@outlook.com>
2024-09-20 16:17:26 +1000

    Added BaseRepository, successfully setup pgadmin

---
+++ LolMatchFilterNew.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolMatchFilterNew.Domain.Interfaces.IGenericRepositories
+{
+    public interface IGenericRepository<in T> where T : class
+    {
+
+    }
+}
---
+++ LolMatchFilterNew.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LolMatchFilterNew.Domain.Interfaces.IGenericRepositories;
+
+namespace LolMatchFilterNew.Infrastructure.Repositories
+{
+    internal class BaseRepository : IGenericRepository
+    {
+        public async Task<
+
+    }
+}

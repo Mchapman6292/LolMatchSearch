82e0249efb92fdce135b5b0c9aeed54f6e9dbbaf
Mchapman <mchapman6292@outlook.com>
2024-10-06 22:46:24 +1000

    Fixed url encoding issues in QueryBuilder

--- LolMatchFilterNew.Application/QueryBuilders/LeaguepediaQueryService.cs
+++ LolMatchFilterNew.Application/QueryBuilders/LeaguepediaQueryService.cs
@@ -11,26 +11,26 @@ namespace LolMatchFilterNew.Application.QueryBuilders.LeaguepediaQueryService
     public class LeaguepediaQueryService : ILeaguepediaQueryService
     {
 
-        // Example tournamentName formats "LEC 2024 Season Finals", "LEC 2024 Spring",
+        // Example tournament format "LEC 2023 Summer Season",
         private const string BaseUrl = "https://lol.fandom.com/api.php";
 
-        // leagueAbbreviation: Replaces tournamentName and should be the short form of the league (e.g., "LEC").
+     
         public string BuildLeaguepediaQuery(string tournamentName, int limit = 480, int offset = 0)
         {
+            // Ensure the tournament name is properly escaped/ url encoded.
+
             var query = HttpUtility.ParseQueryString(string.Empty);
             query["action"] = "cargoquery";
+            query["format"] = "json";
             query["tables"] = "ScoreboardGames=SG,ScoreboardPlayers=SP";
             query["join_on"] = "SG.GameId=SP.GameId";
-            query["fields"] = "SG.GameId, SG.DateTime UTC, SG.Tournament, SG.Team1, SG.Team2, " +
+            query["fields"] = "SG.GameId, SG.DateTime_UTC, SG.Tournament, SG.Team1, SG.Team2, " +
                               "SG.Winner, SG.Team1Picks, SG.Team2Picks";
-                              
             query["where"] = $"SG.Tournament = '{tournamentName}'";
             query["group_by"] = "SG.GameId";
             query["order_by"] = "SG.DateTime_UTC ASC";
             query["limit"] = limit.ToString();
             query["offset"] = offset.ToString();
-            query["format"] = "json";
-
             return $"{BaseUrl}?{query}";
         }
     }
